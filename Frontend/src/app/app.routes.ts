abebc6952ca84c2672282f247b55d8dd0888d57f
Martina Kay Basquez <martinabasquez@gmail.com>
2024-10-16 13:31:14 -0500

    routing stuff

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { DisplayRecipeComponent } from './components/display-recipe/display-recipe.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 
 export const routes: Routes = [
+    {path:"", component:HomeComponent},
     {path:"Recipe/:id", component: DisplayRecipeComponent}, 
-    {path: "**", component: AppComponent}
-
+    {path:"Recipe", component:RecipeFormComponent},
+    {path:"MyRecipes", component:RecipeListComponent},
+    {path: "**", component:PageNotFoundComponent} 
 ];
