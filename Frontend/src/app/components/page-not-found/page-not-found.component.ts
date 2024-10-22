370673165c5b711f1c8e640e0311bd09ce6f5378
Wren <grahamgrasley@gmail.com>
2024-10-22 17:02:10 -0400

    404 styling
    add favicon

--- Frontend/src/app/components/page-not-found/page-not-found.component.ts
+++ Frontend/src/app/components/page-not-found/page-not-found.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
 export class PageNotFoundComponent {
+	taglines: string[] = [
+		"Sorry loser, try again!",
+		"Oopsies",
+		"Skill issue",
+		"Womp womp",
+		"OwO wats dis?",
+		"Server did a fucky-wucky"
+	];
 
+	getTagline() : string {
+		return this.taglines[Math.floor(Math.random() * this.taglines.length)];
+	}
 }
