9a2f577a7847d2be01726c7341d5b0b82076a398
Wren <grahamgrasley@gmail.com>
2024-10-23 15:03:46 -0400

    add sorting

--- Frontend/src/app/components/recipe-list/recipe-list.component.ts
+++ Frontend/src/app/components/recipe-list/recipe-list.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { Recipes } from '../../models/recipes';
 import { HttpClient } from '@angular/common/http';
+import { MatSortModule, Sort } from '@angular/material/sort';
 import { IngredientService } from '../../services/ingredient.service';
 import { RouterLink } from '@angular/router';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatSortModule],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
   recipeList: Recipes [] = [];
+  sortedRecipes: Recipes[] = [];
 
   constructor(private http:IngredientService) {}
 
@@ -21,7 +24,10 @@ export class RecipeListComponent {
   }
 
   updateRecipes() : void {
-    this.http.getRecipes().subscribe(response => this.recipeList = response);
+    this.http.getRecipes().subscribe(response => {
+		this.recipeList = response;
+		this.sortedRecipes = [...this.recipeList];
+	});
   }
 
   removeRecipe(id: number) : void {
@@ -30,5 +36,33 @@ export class RecipeListComponent {
 
   getEditLink(id: number) : string {
 	return "/Recipe/" + id + "/edit";
+  }
+
+  sortChanged(sort: Sort) {
+	const data = [...this.recipeList];
+    if (!sort.active || sort.direction === '') {
+      this.sortedRecipes = data;
+      return;
+    }
+
+    this.sortedRecipes = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+		switch (sort.active) {
+			case 'name':
+			return this.compare(a.name, b.name, isAsc);
+			case 'calories':
+			return this.compare(a.calories, b.calories, isAsc);
+			case 'carbs':
+			return this.compare(a.carbs, b.carbs, isAsc);
+			case 'priority':
+			return this.compare(a.priority, b.priority, isAsc);
+			default:
+			return 0;
+		}
+	});
+  }
+
+  compare(a: number | string, b: number | string, isAsc: boolean): number {
+	return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
 }
